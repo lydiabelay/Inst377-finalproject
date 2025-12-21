5bebfb79640848c6a27dbcfb5127b9b27f79605b
lydiabelay <156359647+lydiabelay@users.noreply.github.com>
2025-12-21 18:35:02 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 function clientFile(fileName) {
