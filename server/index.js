b67927cf27a17b791b6fe5108186cf8ddceacdbc
lydiabelay <156359647+lydiabelay@users.noreply.github.com>
2025-12-21 05:59:35 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("Server Starting...");
+const express = require("express");
+const path = require("path");
+
+const app= express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (req, res) => { res.sendFile( path.join(__dirname, "..","public","client","index.html")
+  );
+                        });
+app.get("/feature", (req,res) => { 
+  res.sendFile (
+  path.join(__dirname, "..", "public","client","feature.html")
+);
+ });
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
